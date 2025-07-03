b12bbcff63f6c84d690408480f58acababfc0096
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-07-03 20:16:11 +0900

    style: 홈 페이지 레이아웃 및 환영 메시지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <main className="flex items-center justify-center bg-gray-900 min-h-screen">
+      <div className="bg-gray-700 rounded-lg shadow-lg p-6 w-full max-w-lg">
+        <h2 className="text-white text-2xl font-bold mb-4 text-center">
+          Welcome to Photo Store
+        </h2>
+        <p className="mb-6 text-lg text-center text-white">
+          Sign in to upload and save your favorite photos.
+        </p>
+        {/* AuthForm */}
+      </div>
+    </main>
+  );
 }
