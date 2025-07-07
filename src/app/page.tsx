0f6c50a93df2eaddd498480e9eddf634d6e26e22
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-07-07 14:28:22 +0900

    chore: 이미지 파일 집어넣기 프로젝트 초기 셋팅

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center w-full h-screen">
+      <h1 className="text-2xl font-semibold">Next.js Photos</h1>
+    </main>
+  );
+}
